8e1de83518163da5452ae1f534d556bb67cf70f4
KhoDis <khodzhayarov.a@gmail.com>
2025-07-15 15:03:48 +0300

    test: make test unpassable

--- test/about.test.tsx
+++ test/about.test.tsx
@@ -6,6 +6,6 @@ describe('About', () => {
     it('Should render author name', () => {
         const { container } = render(<About />);
 
-        expect(container.textContent).toBe('AboutAuthor: Вишняков Владимир');
+        expect(container.textContent).toBe('AboutAuthor: Бебебе, тест не пройдёт теперь, мухахахахха');
     });
 });
